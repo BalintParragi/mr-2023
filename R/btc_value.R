cc4015cbd23d22b802a259ca4526b94cf01ddcda
Balint <parragi.balint@gmail.com>
2023-05-22 14:42:12 +0000

    Add extra functions to complete package

---
+++ R/btc_value.R
@@ -0,0 +1,24 @@
+#' Retrieve most recent BTC value in USD
+#' @param attempt 1
+#' @return num value of 1 BTC in USD
+#' @examples
+#' get_btcusdt()
+#' @export
+#' @importFrom binancer binance_coins_prices
+#' @import checkmate, logger, data.table
+
+get_btcusdt <- function(attempt = 1){
+  tryCatch({
+    btcusdt <-  binance_coins_prices()[symbol == 'BTC', usd]
+    assert_number(btcusdt,lower = 15000,upper = 100000)
+  },error = function(e) {
+    log_error(e$message)
+    if(attempt > 5){
+      stop("Give up")
+    }
+    Sys.sleep(1 + attempt^2)
+    get_btcusdt(attempt = attempt + 1)
+  })
+  log_info('The current Bitcoin price is ${btcusdt}')
+  btcusdt
+}
---
+++ R/exchange_rate.R
@@ -0,0 +1,20 @@
+#' Retrieve USD/EUR exchange rate using API
+#' @return num value of 1 USD in EUR
+#' @examples
+#' get_usdeur()
+#' @export
+#' @importFrom jsonlite fromJSON
+#' @import checkmate, logger
+
+get_usdeur <- function(){
+  tryCatch({
+    usdeur <- fromJSON('https://api.exchangerate.host/latest?base=USD&symbols=EUR')$rates$EUR
+    assert_number(usdeur,lower = 0.1,upper = 2)
+  },error = function(e) {
+    log_error(e$message)
+    Sys.sleep(1)
+    get_usdeur()
+  })
+  log_info('1 USD currently costs {usdeur} EUR')
+  usdeur
+}
